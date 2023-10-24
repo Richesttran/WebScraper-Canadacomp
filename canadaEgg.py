d0e5d02d7bfb4a548f76f3764299754647b50724
Richest <rtran2477@conestogac.on.ca>
2023-10-24 15:00:54 -0400

    Turned code into a class

--- canadaEgg.py
+++ canadaEgg.py
@@ -1,34 +1,37 @@
 
-def remove_dolla(string):
-  if "$" in string: 
-    modifiedstring=string.replace("$","")
-   
-    return modifiedstring
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-#new change testasdasd
+class PriceFinder:
+  def remove_dolla(string):
+    if "$" in string: 
+      modifiedstring=string.replace("$","")
+    
+      return modifiedstring
 
-driver = webdriver.Chrome()
+  #new change testasdasd
+  def findPrice(self):
+    driver = webdriver.Chrome()
 
-driver.get("https://www.canadacomputers.com/product_info.php?cPath=4_64&item_id=226995")  # Replace with the URL of the web page
+    driver.get("https://www.canadacomputers.com/product_info.php?cPath=4_64&item_id=226995")  # Replace with the URL of the web page
 
-meta_element = driver.find_element(By.CSS_SELECTOR, 'meta[name="price"]')
+    meta_element = driver.find_element(By.CSS_SELECTOR, 'meta[name="price"]')
 
-canada_value = meta_element.get_attribute("content")
+    canada_value = meta_element.get_attribute("content")
 
-driver.get("https://www.newegg.ca/amd-ryzen-7-7700x-ryzen-7-7000-series/p/N82E16819113768?Item=N82E16819113768")  # Replace with the URL of the web page
+    driver.get("https://www.newegg.ca/amd-ryzen-7-7700x-ryzen-7-7000-series/p/N82E16819113768?Item=N82E16819113768")  # Replace with the URL of the web page
 
-li_element = driver.find_element(By.CSS_SELECTOR, 'li.price-current')
-newegg_value = li_element.text
+    li_element = driver.find_element(By.CSS_SELECTOR, 'li.price-current')
+    newegg_value = li_element.text
 
-canada_result = float(remove_dolla(canada_value))
-newegg_result = float(remove_dolla(newegg_value))
-                     
-print("Price for canada computers:", canada_value, " new egg price ",newegg_value )
-if canada_result>newegg_result:
-  print("canada computer is the better deal",canada_value)
-elif canada_result<newegg_result:
-  print("newegg is the better deal",newegg_value)
-else:
-  print("They are the same :(")
-driver.quit()
+    canada_result = float(self.remove_dolla(canada_value))
+    newegg_result = float(remove_dolla(newegg_value))
+
+    print("Price for canada computers:", canada_value, " new egg price ",newegg_value )
+    if canada_result>newegg_result:
+      print("canada computer is the better deal",canada_value)
+    elif canada_result<newegg_result:
+      print("newegg is the better deal",newegg_value)
+    else:
+      print("They are the same :(")
+    driver.quit()
+
