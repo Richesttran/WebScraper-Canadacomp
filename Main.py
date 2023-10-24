eed5a609422757f9dcc57a5c90fd3179ca2c6e11
Richest <rtran2477@conestogac.on.ca>
2023-10-24 19:06:03 -0400

    can edit and update prices in textfile

--- Main.py
+++ Main.py
@@ -1,6 +1,30 @@
 
 from canadaEgg import PriceFinder
 item_detail = PriceFinder()
-with open('Wishlist.txt','r') as file:
-  for line in file:
-   print(item_detail.findPriceOne(line.strip()))
+product_price=[]      # square brackets are lists the round ones are tupples
+iterator=0
+# put the item price and name into a list (i could have written and read at the same time but i did this for readability sake)
+
+product_price=item_detail.findPriceOne("Wishlist.txt")
+
+# open file as writable and readable
+with open('ProductPrice.txt','r+') as file:
+  for product in product_price:                    
+    for line in file:                                        # iterate each product in the text file
+
+      if line.find(product[0]) != -1:                      # check if it has an existing line
+
+        if line.find(product[1]) != -1:
+          print("no price change for the "+ product[0])
+        else:
+          file.writelines(product[0] + product[1]+"\n")            # overwrite the existing line with new price
+        break
+      elif not file.readline():                                     # check if at end of line
+        file.write(product[0] + product[1]+"\n")                      # write the new line  
+        break
+
+
+
+          
+
+print(product_price)
